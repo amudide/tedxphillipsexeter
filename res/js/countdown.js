76d65276d18485558af03e6ce5fdbea7361291e5
amudide <amudide@gmail.com>
2021-04-16 12:39:03 -0400

    time

--- res/js/countdown.js
+++ res/js/countdown.js
@@ -1,5 +1,5 @@
 $('.countdown-timer').countdown({
-    date: "April 17, 2021 10:00:00",
+    date: "April 17, 2021 20:00:00",
 
     render: function(data) {
         $(".days .value").html(this.leadingZeros(data.days, 2));
